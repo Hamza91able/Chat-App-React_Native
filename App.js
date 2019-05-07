04a95121d205a1ebd42ad9a7ba5914c519662666
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2019-05-07 20:25:15 +0500

    Added basic header to Chat

--- App.js
+++ App.js
@@ -12,10 +12,14 @@ import {
   Icon,
   Button,
   View,
+  Left,
   Text,
   Body,
   Title,
   Subtitle,
+  Thumbnail,
+  ListItem,
+  CardItem,
 } from 'native-base'
 import { fetchChats } from './constants/api';
 import {
@@ -40,6 +44,8 @@ export default class App extends React.Component {
   state = {
     loading: false,
     chats: [],
+    Avatar: "https://static.scientificamerican.com/blogs/cache/file/D4C437D8-244E-4582-BF1A158A7330AD33_source.jpg?w=590&h=800&B039F14E-190A-42BE-BA73A8D60DD542A5",
+    Name: 'Hamza',
   }
 
   async componentWillMount() {
@@ -96,6 +102,7 @@ const TabNavigator = createMaterialTopTabNavigator(
   }
 );
 
+
 const ContactStack = createStackNavigator({
   Home: {
     screen: TabNavigator,
@@ -141,7 +148,35 @@ const ContactStack = createStackNavigator({
   },
   Chat: {
     screen: Chat,
-  }
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle:
+          <CardItem style={{ backgroundColor: '#128c7e' }}>
+            <Left>
+              <Thumbnail small source={{ uri: `${new App().state.Avatar}` }} />
+              <Body>
+                <Text style={{ color: 'white' }}>{new App().state.Name}</Text>
+                <Text note style={{ color: 'white' }}>Last Seen: value</Text>
+              </Body>
+            </Left>
+          </CardItem>,
+        headerRight: <React.Fragment>
+          <Button style={{ marginTop: 10 }} transparent>
+            <Icon style={{ color: 'white' }} name='more' />
+          </Button>
+        </React.Fragment>,
+        headerStyle: {
+          backgroundColor: '#128c7e',
+          elevation: null,
+          height: 65,
+        },
+        headerTitleStyle: {
+          color: 'white'
+        }
+      }
+    }
+  },
+
 })
 
 const DashboardStackNavigator = createStackNavigator({
